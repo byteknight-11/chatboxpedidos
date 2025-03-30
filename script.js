866a8fe1c1ec9f6f98b88c3b7b5c2deba3dac62d
Leonardo De Jesús Eljadue Silva <leo.eljadues@gmail.com>
2025-03-30 18:29:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function sendMessage() {
 
     chat.innerHTML += `<p><strong>Tú:</strong> ${input}</p>`;
 
-    fetch("http://127.0.0.1:5000/chat", {
+    fetch("https://chatbot-pedidos.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input })
